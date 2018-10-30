4b5cc3f1b270e1071ca2c959bf2a0342b20046e7
ninazu <ninazu@ukr.net>
2018-10-30 11:42:03 +0200

    #init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "C"
 import (
+	"fmt"
 	"github.com/janne/bcm2835"
 	"time"
 )
@@ -11,6 +12,8 @@ const PIN = bcm2835.Pin11
 const (
 	BCM2835_GPIO_FSEL_INPT = 0
 	BCM2835_GPIO_FSEL_OUTP = 1
+	READ_OPCODE            = 0x33
+	WRITE_OPCODE           = 0xD5
 )
 
 func delay(microSec time.Duration) {
@@ -69,6 +72,27 @@ func write(b byte) {
 	}
 }
 
+func writeCode(data []byte) bool {
+	sendR()
+
+	if !waitP() {
+		return false
+	}
+
+	send(WRITE_OPCODE)
+
+	for bit := 0; bit < 8; bit++ {
+		write(data[bit])
+	}
+
+	delay(16000)
+
+	sendR()
+	waitP()
+
+	return true
+}
+
 func sendR() {
 	down()
 	delay(1024)
@@ -76,14 +100,14 @@ func sendR() {
 	delay(1)
 }
 
-func waitP() int {
+func waitP() bool {
 	count := 10000
 
 	for bcm2835.GpioLev(PIN) != 0 {
 		count--
 
 		if count < 0 {
-			return 0
+			return false
 		}
 	}
 	for bcm2835.GpioLev(PIN) == 0 {
@@ -92,10 +116,10 @@ func waitP() int {
 
 	delay(1)
 
-	return 1
+	return true
 }
 
-func recv() byte {
+func receive() byte {
 	var b byte = 0
 
 	for bit := 0; bit < 8; bit++ {
@@ -118,6 +142,63 @@ func recv() byte {
 	return b
 }
 
+func readCode() (bool, []byte) {
+	sendR()
+
+	if !waitP() {
+		return false, nil
+	}
+
+	send(READ_OPCODE)
+	var data = make([]byte, 8)
+
+	for bit := 0; bit < 8; bit++ {
+		data[bit] = receive()
+	}
+
+	return true, data
+}
+
+func readButton() []byte {
+	var data []byte
+
+	for {
+		if result, data := readCode(); result {
+			fmt.Printf("Read: %x\n", data)
+			break
+		} else {
+			fmt.Printf(".\n")
+		}
+
+		delay(500000)
+	}
+
+	return data
+}
+
+func writeButton(data []byte) bool {
+	if writeCode(data) {
+		fmt.Printf("Write: %x\n", data)
+	} else {
+		fmt.Printf("Writing failed \n")
+
+		return false
+	}
+
+	return true
+}
+
+func copyButton() {
+	data := readButton()
+
+	if !writeButton(data) {
+		return
+	}
+
+	fmt.Printf("Verifying\n")
+	readButton()
+}
+
 func main() {
 	err := bcm2835.Init()
 
@@ -131,8 +212,5 @@ func main() {
 
 	defer bcm2835.Close()
 
-	for {
-
-		delay(500000)
-	}
+	readButton()
 }
